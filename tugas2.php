37e6d04e727a32494ffb4e20c18b1f83e1af555f
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-10-07 10:22:44 +0700

    Add files via upload

---
+++ tugas2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<?php
+	$menu1 = "Cireng";
+	$menu2 = "Es Duren";
+	$menu3 = "Es Milo";
+	$menu4 = "Es Teh";
+	$menu5 = "Piscok";
+
+	$harga1 = "Rp 1000";
+	$harga2 = "RP 15000";
+	$harga3 = "Rp 20000";
+	$harga4 = "Rp 5000";
+	$harga5 = "Rp 1000";
+
+	echo"<h1>DAFTAR MENU</h1>";
+	echo"<table border = '1'>
+
+		<tr>
+		<td>No</td>
+		<td>Nama Menu</td>
+		<td>Harga</td>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>".$menu1."</td>
+		<td>".$harga1."</td>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>".$menu2."</td>
+		<td>".$harga2."</td>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>".$menu3."</td>
+		<td>".$harga3."</td>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>".$menu4."</td>
+		<td>".$harga4."</td>
+		</tr>
+		<tr>
+		<td>1</td>
+		<td>".$menu5."</td>
+		<td>".$harga5."</td>
+		</tr>
+	</table>";
+		
+
+?>
+</body>
+</html>
